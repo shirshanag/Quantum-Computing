da1fa2f36322e439b53e3542fb4fc7cb2fe4c487
shirshanag <154455132+shirshanag@users.noreply.github.com>
2025-06-21 21:28:52 +0530

    Update Bell State Entanglement.py

--- Bell State Entanglement.py
+++ Bell State Entanglement.py
@@ -1,17 +1,30 @@
+#Import the Libraries
+
 from qiskit import QuantumCircuit
 from qiskit_aer import Aer
 from qiskit.visualization import plot_histogram
 import matplotlib.pyplot as plt
 
-qc = QuantumCircuit(1, 1)
-qc.h(0)
-qc.measure(0, 0)
+#Two qubit Quantum Circuit
+qc=QuantumCircuit(2)
 
-sim = Aer.get_backend('aer_simulator')
-job = sim.run(qc, shots=1000)
-results = job.result()
-counts = results.get_counts(qc)
-print(counts)
-plot_histogram(counts)
+#Apply Quantum Gates
+
+qc.h(0)
+qc.cx(0,1)
+qc.measure_all()
+
+#Simulate the result
+
+sim=Aer.get_backend('aer_simulator')
+job=sim.run(qc,shots=1000)
+results=job.result()
+
+#Draw the Circuit
+
 qc.draw('mpl')
 
+#Print and Plot the Counts 
+
+print(counts)
+plot_histogram(counts)
